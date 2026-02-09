64d912dfbe89600426fd5dc8ac1b4bb52a33ba71
Daniel Damiba <ddamiba@c2r8s1.42lisboa.com>
2026-02-09 15:42:07 +0000

    slight changes in an attempt to optimize further

--- utils2.c
+++ utils2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniel <daniel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ddamiba <ddamiba@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 02:10:19 by daniel            #+#    #+#             */
-/*   Updated: 2026/01/31 01:23:27 by daniel           ###   ########.fr       */
+/*   Updated: 2026/02/09 15:41:31 by ddamiba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,11 @@ void	usleep_manager(t_data *master, int time)
 	long	elapsed;
 
 	start = get_time();
-	while (1)
+	(void)elapsed;
+	while (get_time() - start < time)
 	{
 		if (check_dead(master))
 			return ;
-		elapsed = get_time() - start;
-		if (elapsed >= time)
-			break ;
 		usleep(500);
 	}
 }
